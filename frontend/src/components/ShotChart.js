e6ca8c983c95b3e23b329b4774e1b150cec5ea4b
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2024-09-26 13:04:58 -0700

    fixed skeleton styling

--- frontend/src/components/ShotChart.js
+++ frontend/src/components/ShotChart.js
@@ -19,39 +19,6 @@ function ShotChart({ imageBase64, playerName, statistics, favoriteShots }) {
         <div className="w-full md:w-2/3">
           <img src={imageUrl} alt="Shot Chart" className="w-full h-auto" />
         </div>
-        {/* <div className="w-full md:w-1/3 mt-6 md:mt-0">
-          <div className="p-6 pt-2 pb-2">
-            <h3 className="text-2xl font-semibold mb-4 text-center">Statistics</h3>
-            <ul className="space-y">
-              {[
-                { label: 'Total Shot Attempts', value: statistics.total_shots },
-                { label: 'Field Goal Percentage', value: `${(statistics.total_make_percentage * 100).toFixed(1)}%` },
-                { label: '3-Point Attempts', value: statistics.three_point_attempts },
-                { label: '3-Point Percentage', value: `${(statistics.three_point_percentage * 100).toFixed(1)}%` },
-                { label: '2-Point Attempts', value: statistics.two_point_attempts },
-                { label: '2-Point Percentage', value: `${(statistics.two_point_percentage * 100).toFixed(1)}%` },
-                { label: 'Clutch FG Percentage', value: `${(statistics.clutch_fg_percentage * 100).toFixed(1)}%` },
-                { label: 'Avg. Shot Distance', value: `${statistics.average_shot_distance.toFixed(1)} ft` },
-              ].map((stat, index) => (
-                <li key={index} className="flex justify-between hover:bg-gray-700 p-2 rounded transition-default">
-                  <span>{stat.label}:</span>
-                  <span>{stat.value}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="p-6 rounded-lg">
-            <h3 className="text-2xl font-semibold mb-4 text-center">Favorite Shots</h3>
-            <ul className="space-y">
-              {Object.entries(favoriteShots).map(([actionType, count], index) => (
-                <li key={index} className="flex justify-between hover:bg-gray-700 p-2 rounded transition-default">
-                  <span>{actionType}</span>
-                  <span>{count}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div> */}
       </div>
       {/* Statistics section */}
       <div className="pt-10 p-6 grid grid-cols-1 md:grid-cols-3 justify-items-center md:max-w-[85%] mx-auto">
